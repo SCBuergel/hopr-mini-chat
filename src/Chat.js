86e457214e268b61314651e39706adaec13e2f14
Sebastian Bürgel <s.buergel@gmail.com>
2022-06-10 14:25:30 +0200

    minimal components

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,54 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        contact: { this.props.address }
+      </div>
+    )
+  }
+}
+
+class Message extends React.Component {
+  render() {
+    return (
+      <div>
+        message: { this.props.message }
+      </div>
+    )
+  }
+}
+
+class Sender extends React.Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="recipient">recipient:</label>
+        <input type="text" id="recipient" defaultValue="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" /> <br />
+        <label htmlFor="message">message:</label>
+        <textarea maxLength="600" id="message" defaultValue="HellowWorld" /> <br />
+        <button>send</button>
+        
+        recipient: { this.props.message }
+      </div>
+    )
+  }
+}
+
 function Chat() {
   const params = new URLSearchParams(window.location.search)
 
   return (
     <div>
       HOPR mini chat
+      <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
+      <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
+      <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
+      <Message message="Hello World!" />
+      <Message message="Hello World!" />
+      <Message message="Hello World!" />
+      <Sender />
       { params.get("securityToken") }
     </div>
   );
