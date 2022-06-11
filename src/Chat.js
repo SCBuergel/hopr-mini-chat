80dc6a142ff48d02e920d87613a907752ecb9fbf
Sebastian Bürgel <s.buergel@gmail.com>
2022-06-11 23:13:24 +0200

    receive msgs

--- src/Chat.js
+++ src/Chat.js
@@ -14,7 +14,8 @@ class Message extends React.Component {
   render() {
     return (
       <div>
-        message: { this.props.message }
+        received: { this.props.message.ts} <br />
+        message: { this.props.message.msg }
       </div>
     )
   }
@@ -43,8 +44,6 @@ class Sender extends React.Component {
         <label htmlFor="message">message:</label>
         <textarea maxLength="600" id="message" defaultValue={message} /> <br />
         <button id="btn" value={message} onClick={this.submitMessage}>send</button>
-
-        recipient: { this.props.message }
       </div>
     )
   }
@@ -53,13 +52,34 @@ class Sender extends React.Component {
 class Chat extends React.Component {
   constructor(props) {
     super(props)
-    const params = new URLSearchParams(window.location.search)
     this.postMessage = this.postMessage.bind(this)
     this.apiCall = this.apiCall.bind(this)
+    this.handleReceivedMessage = this.handleReceivedMessage.bind(this)
+    const params = new URLSearchParams(window.location.search)
+    const securityToken = params.get("securityToken")
+    const wsUrl = new URL(params.get("wsEndpoint"))
+    wsUrl.search = `?apiToken=${securityToken}`
+    const ws = new WebSocket(wsUrl)
+    ws.addEventListener('message', this.handleReceivedMessage)
     this.state = {
       httpEndpoint: params.get("httpEndpoint"),
-      wsEndpoint: params.get("wsEndpoint"),
-      securityToken: params.get("securityToken")
+      ws: ws,
+      securityToken: securityToken,
+      messages: []
+    }
+  }
+
+  handleReceivedMessage(e) {
+    try {
+      const data = JSON.parse(e.data)
+      console.log('WebSocket Data', data)
+      if (data.type === 'message') {
+        this.setState({ 
+          messages: [...this.state.messages, data]
+        })
+      }
+    } catch (err) {
+      console.log(err)
     }
   }
 
@@ -93,15 +113,16 @@ class Chat extends React.Component {
   }
 
   render() {
+    const msgs = this.state.messages.map( (msg, index)  =>
+      <Message key={index.toString()} message={msg} />
+    )
     return (
       <div>
         HOPR mini chat
         <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
         <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
         <Contact address="16Uiu2HAkucqh4zJJPdnBiA9nw8WGkm29LoNg7wSC9kVMn5BvhdrU" />
-        <Message message="Hello World!" />
-        <Message message="Hello World!" />
-        <Message message="Hello World!" />
+        {msgs}
         <Sender recipient={this.recipient} messageText={this.messageText} onSubmit={this.postMessage} />
       </div>
     );
